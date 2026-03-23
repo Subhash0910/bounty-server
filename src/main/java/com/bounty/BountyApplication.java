9e1cd5e28042fbd2690ffabf0d9281b24244e19a
Subhash <subhashvenkata1430@gmail.com>
2026-03-24 00:58:45 +0530

    init: pom.xml, BountyApplication, application.properties

---
+++ src/main/java/com/bounty/BountyApplication.java
@@ -0,0 +1,11 @@
+package com.bounty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BountyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BountyApplication.class, args);
+    }
+}
